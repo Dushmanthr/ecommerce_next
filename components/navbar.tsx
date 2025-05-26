9ac558bc3e059f325f4fef4403c471471a3ca9a8
Dushmantha Ranathunga <e102.mavicsoft@gmail.com>
2025-05-26 23:18:49 +0530

    feature: navbar feature developed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,16 +5,19 @@ export const Navbar = () => {
     return (
         <nav className='sticky top-0 z-50 bg-white shadow'>
             <div className='container mx-auto flex items-center justify-between px-4 py-4'>
-                <Link href='/'>My Ecommerce</Link>
-            </div>
+                <Link href='/' className = "hover: text-blue-600">My Ecommerce</Link>
 
             <div className='hidden-md flex space-x-6'>
                 <Link href= "/" className='hover: text-blue-600'>Home</Link>
                 <Link href= "/products" className='hover: text-blue-600'>Products</Link>
                 <Link href= "/checkouts" className= 'hover: text-blue-600'>Checkouts</Link>
             </div>
-
             <div className="flex item-center space-x-4"></div>
+            </div>
+
+           
+
+            
         </nav>
     )
 }
