3a0b3152e229436c42c9a1eca5d5924776f8b099
Dushmantha Ranathunga <e102.mavicsoft@gmail.com>
2025-05-26 23:07:24 +0530

    build: project set up

---
+++ components/navbar.tsx
@@ -0,0 +1,20 @@
+
+import Link from 'next/link'
+
+export const Navbar = () => {
+    return (
+        <nav className='sticky top-0 z-50 bg-white shadow'>
+            <div className='container mx-auto flex items-center justify-between px-4 py-4'>
+                <Link href='/'>My Ecommerce</Link>
+            </div>
+
+            <div className='hidden-md flex space-x-6'>
+                <Link href= "/" className='hover: text-blue-600'>Home</Link>
+                <Link href= "/products" className='hover: text-blue-600'>Products</Link>
+                <Link href= "/checkouts" className= 'hover: text-blue-600'>Checkouts</Link>
+            </div>
+
+            <div className="flex item-center space-x-4"></div>
+        </nav>
+    )
+}
