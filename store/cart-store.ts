ea716348a06d51b267a6ea9584a871ee30fb374b
Dushmantha Ranathunga <e102.mavicsoft@gmail.com>
2025-06-10 01:38:19 +0530

    feat: store quantity implementation

--- store/cart-store.ts
+++ store/cart-store.ts
@@ -1,3 +1,6 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
 export interface CartItem {
     id: string;
     name: string;
@@ -6,3 +9,24 @@ export interface CartItem {
     quantity: number;
 }
 
+interface CartStore {
+    items:CartItem[];
+    addItem: (item: CartItem) => void;
+}
+
+export const useCartStore = create<CartStore>()(persist((set) =>({
+    items:[],
+    addItem: (item) => set((state) => {
+        const existing = state.items.find((i) => i.id === item.id);
+
+        if(existing){
+            return{
+                items: state.items.map((i) => i.id === item.id ? {...i, quantity: i.quantity + item.quantity}
+                :i
+            ),
+            }
+        }
+        return {items: [...state.items, item] };
+    })
+}), {name:'cart'})
+)
