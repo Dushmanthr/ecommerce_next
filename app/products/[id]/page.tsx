eb1cadce08628fd82ef5046df4526a56e205e208
Dushmantha Ranathunga <e102.mavicsoft@gmail.com>
2025-06-15 01:01:36 +0530

    fix: params compatibility fix

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,7 +1,12 @@
 import { stripe } from "@/lib/stripe";
 import { ProductDetails } from "@/components/product-detail";
 
-export default async function ProductPage({params}: {params: {id: string}}) {
+type Props = {
+    params: {id: string};
+    searchParams?: {[key: string]: string | string[] | undefined};
+};
+
+export default async function ProductPage({params}: Props) {
 
     const product = await stripe.products.retrieve(params.id, {
         expand: ['default_price']
