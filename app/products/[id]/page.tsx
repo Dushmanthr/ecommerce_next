e036c0051d61bf991d233a7d8530e3a8949b56a9
Dushmantha Ranathunga <e102.mavicsoft@gmail.com>
2025-06-15 01:37:24 +0530

    fix: fix deploying error

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,16 +1,21 @@
 import { stripe } from "@/lib/stripe";
 import { ProductDetails } from "@/components/product-detail";
 
+// Define props type for the dynamic route
 type Props = {
-    params: {id: string};
-    searchParams?: {[key: string]: string | string[] | undefined};
+  params: { id: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
 };
 
-export default async function ProductPage({params}: Props) {
+// Define type for the Stripe product
 
-    const product = await stripe.products.retrieve(params.id, {
-        expand: ['default_price']
-    })
-    const plainProduct = JSON.parse(JSON.stringify(product));
-return <ProductDetails product={plainProduct}/>;
+
+export default async function ProductPage({ params }: Props) {
+  const product = await stripe.products.retrieve(params.id, {
+    expand: ["default_price"],
+  });
+  return <ProductDetails product={product} />;
 }
+
+
+
